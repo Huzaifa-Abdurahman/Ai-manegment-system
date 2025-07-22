3b89f0af2661d6dcbae4d0f2f05a3fc8cbb6360b
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2025-07-22 17:30:45 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
 import { AppLayout } from '@/components/app-layout';
-import { Toaster } from "@/components/ui/toaster"
+import ToasterWrapper from '@/components/ToasterWrapper';
 
 export const metadata: Metadata = {
   title: 'Punjab Soap',
@@ -19,14 +19,14 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
-        
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700;display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className="font-body antialiased">
-        <AppLayout>
-          {children}
-        </AppLayout>
-        <Toaster />
+        <AppLayout>{children}</AppLayout>
+        <ToasterWrapper />
       </body>
     </html>
   );
