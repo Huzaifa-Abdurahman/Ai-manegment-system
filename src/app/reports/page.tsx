2ac35f2ea8ffa5a753a0635ddabf9f07a6bf256c
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2025-07-22 17:26:00 +0500

    Update page.tsx

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import DeliveryReport from "@/components/delivery-report";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { FileText } from "lucide-react";
